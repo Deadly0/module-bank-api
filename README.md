31652531aee40288ef99445254312eec66a86e5f
Deadly0 <xdeadlyx@ya.ru>
2017-07-17 23:49:49 +0300

    First version.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# module-bank-api
-Wrapper for ModuleBank API for node.js
+Wrapper for [ModuleBank API](https://api.modulbank.ru/) for Node.js
+
+# Install
+`npm i module-bank-api -S`
+
+# Usage
+
+## Import
+```javascript
+let ModuleBankApi = require('./api');
+```
+
+## API object
+
+```javascript
+let api = new ModuleBankApi(
+    'sandboxtoken', // Auth token 
+    true // true - sandbox mode, false - production mode
+);
+```
+
+## API methods
+
+* `api.getAccountInfo(callback)` - returns user accounts (https://api.modulbank.ru/data/#_5)
+* `api.getOperationHistory(bankAccountId, callback)` - returns operation history for the account (https://api.modulbank.ru/data/#_6)
+* `api.getBalance(bankAccountId, callback)` - returns balance of the account (https://api.modulbank.ru/data/#_7)
+* `api.uploadOperation(operation1c, callback)` - uploads operations in 1C format (https://api.modulbank.ru/load/)
+
+## Callback signature
+
+```javascript
+function callback(error, response) {
+    if (!error) {
+        console.log(response);
+    }
+}
+```
+
+* `error` - error information or `null` if no error happens
+* `response` - response data
+
+## Example
---
+++ api.js
@@ -0,0 +1,78 @@
+let request = require('request');
+
+
+module.exports = class ModuleBankApi {
+	/**
+	 * ModuleBankApi
+	 * https://api.modulbank.ru/auth/
+	 *
+	 * @param token - токен авторизации
+	 * @param sandbox - включить/выключить режим песочницы
+	 */
+	constructor(token, sandbox=false) {
+		this.API_BASE_URL = 'https://api.modulbank.ru/v1/';
+
+		this.token = token;
+		this.sandbox = sandbox;
+	}
+
+	/**
+	 * Получение информации о компаниях и счетах компаний пользователя
+	 * https://api.modulbank.ru/data/#_5
+	 *
+	 * @param callback - результат запроса
+	 */
+	getAccountInfo(callback) {
+		this.sendRequest('account-info', {}, callback);
+	}
+
+	/**
+	 * Просмотр истории операций
+	 * https://api.modulbank.ru/data/#_6
+	 *
+	 * @param bankAccountId - системный идентификатор счета
+	 * @param callback - результат запроса
+	 */
+	getOperationHistory(bankAccountId, callback) {
+		this.sendRequest('operation-history/' + bankAccountId, {}, callback);
+	}
+
+	/**
+	 * Получение баланса по счету
+	 * https://api.modulbank.ru/data/#_7
+	 *
+	 * @param bankAccountId - системный идентификатор счета
+	 * @param callback - результат запроса
+	 */
+	getBalance(bankAccountId, callback) {
+		this.sendRequest('account-info/balance/' + bankAccountId, {}, callback);
+	}
+
+	/**
+	 * Импорт платежек в формате 1С (бета версия)
+	 * https://api.modulbank.ru/load/
+	 *
+	 * @param document
+	 * @param callback - результат запроса
+	 */
+	uploadOperation(document, callback) {
+		this.sendRequest('operation-upload/1c', {document: document}, callback);
+	}
+
+	sendRequest(methodUrl, data={}, callback=function () {}) {
+		let options = {
+			uri: this.API_BASE_URL + methodUrl,
+			method: 'POST',
+			json: data,
+			auth: {'bearer': this.token},
+		};
+
+		if (this.sandbox) {
+			options.headers = {sandbox: 'on'};
+		}
+
+		request(options, function (error, resp) {
+			callback(error, resp && resp.body);
+		});
+	}
+};
